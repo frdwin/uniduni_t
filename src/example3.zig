6fb25b2d0cfc62fc556d17ec8a7f7c9e6557e56e
Frederico Winter <frederico@diaswinter.com.br>
2024-05-13 14:31:53 -0300

    changed example 3

--- src/example3.zig
+++ src/example3.zig
@@ -3,6 +3,6 @@ const Uniduni_t = @import("uniduni_t.zig").Uniduni_t;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    const yellow_string = Uniduni_t.init().yellow().format("This is a yellow string");
-    try stdout.print("{s}\n", .{yellow_string});
+    const bright_yellow_string = Uniduni_t.init().brightYellow().format("This is a bright yellow string");
+    try stdout.print("{s}\n", .{bright_yellow_string});
 }
